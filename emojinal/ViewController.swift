f4113c56d1f5c4bf3d244f5d0ac549c32a70c28e
Jennifer Sterling <Jenniifersterling@gmail.com>
2019-07-12 09:39:55 -0700

    Second Iteration Complete

--- emojinal/ViewController.swift
+++ emojinal/ViewController.swift
@@ -9,7 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBAction func dogBtn(_ sender: Any) {
+        let alertController = UIAlertController(title: "You Chose Dog!", message: "Woof!", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func catBtn(_ sender: Any) {
+        let alertController = UIAlertController(title: "You Chose Cat!", message: "Meow!!", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
 
+    }
+    
+    @IBAction func monkeyBtn(_ sender: Any) {
+        let alertController = UIAlertController(title: "You Chose Monkey!", message: "Ooh Ooh Ah Ah!", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func unicornBtn(_ sender: Any) {
+        let alertController = UIAlertController(title: "You Chose Unicorn!", message: "Neigh ✨!", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
